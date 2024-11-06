7a10b134e2bc778fbce598edd3aaa388f78d3977
keadevs <51330099+keadevs@users.noreply.github.com>
2024-11-06 20:01:49 +0530

    Update Footer.js

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
 			<div className="container margin_60_35">
 				<div className="row">
 					<div className="col-lg-4 col-md-12">
-						<Link to={`/`}>
-							<img className='appLogo' src={appLogo} alt="Something Went Wrong" />
+						<Link to={`https://fly4smiles.com`}>
+							<img className='appLogo' src={appLogo} alt="Fly4smiles" />
 						</Link>
 					</div>
 					<div className="col-lg-4 col-md-4">
@@ -15,16 +15,13 @@ export default function Footer() {
 						<ul className="links">
 							<li><Link to="https://fly4smiles.com/introduction/">About us</Link></li>
 							<li><Link to="https://fly4smiles.com/all-services/">Services</Link></li>
-							{/* <li><Link to="http://keasofttech.com/">FAQ</Link></li>
-							<li><Link to="login.html">Login</Link></li>
-							<li><Link to="register.html">Register</Link></li> */}
 						</ul>
 					</div>
 					<div className="col-lg-4 col-md-4">
 						<h5>Contact with Us</h5>
 						<ul className="contacts">
 							<li><Link to="tel:+91 99233 87272"><i className="icon_mobile"></i> +91 99233 87272</Link></li>
-							<li><Link to="mailto:Support@fly4smiles.com"><i className="icon_mail_alt"></i>Support@fly4smiles.com</Link></li>
+							<li><Link to="mailto:support@fly4smiles.com"><i className="icon_mail_alt"></i>support@fly4smiles.com</Link></li>
 						</ul>
 					</div>
 				</div>
