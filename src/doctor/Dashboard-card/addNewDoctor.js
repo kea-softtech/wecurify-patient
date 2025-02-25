51b262e90aa68606ea37022088af45643bc73781
Roshan Deorukhkar <roshandeorukhkar@gmail.com>
2025-02-25 11:47:46 +0530

    remove otp

--- src/doctor/Dashboard-card/addNewDoctor.js
+++ src/doctor/Dashboard-card/addNewDoctor.js
@@ -24,7 +24,7 @@ export default function AddNewDoctor() {
         else {
             login({ mobile: mobile })
                 .then(response => {
-                    alert(response.data.otp)
+                    // alert(response.data.otp)
                     let item = response.data
                     setLoginData(item)
                     setShowOTP(true)
